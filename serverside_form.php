9a0ac031eda83aad4e303dd94eb6bab3bc5fc322
smash3r <bhubanmagar79@gmail.com>
2023-08-31 18:32:00 +0545

    added

--- serverside_form.php
+++ serverside_form.php
@@ -29,7 +29,20 @@
 </style>
 
 <body>
-    <form onsubmit="validatemail()">
+    <?php
+    if (isset($_GET['submit'])) {
+        $email2 = $_GET['email2'];
+        $validation = "/([a-zA-Z]+@[a-zA-Z]+.[com|org])/";
+        if (preg_match($validation, $email2)) {
+            echo "valid email";
+        } else {
+            echo "invalid email";
+        }
+    }
+
+    ?>
+
+    <form method"get">
         <div class="card">
             <div>
                 <label for="Uname"><strong>Username</strong></label><br />
@@ -50,10 +63,10 @@
                 <input type="text" id="email2" name="email2" required>
             </div><br />
             <div id="validate" style="background-color: red"></div><br />
-            <button type="submit">Submit</button>
+            <button type="submit" name="submit">Submit</button>
         </div>
     </form>
-    <script>
+    <!-- <script>
         const validatemail = () => {
             event.preventDefault();
             var email = document.getElementById("email2").value;
@@ -66,7 +79,7 @@
                 return false;
             }
         }
-    </script>
+    </script> -->
 </body>
 
 </html>
